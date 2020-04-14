5fbd3b7477578a5215499a9df6bc83927e3fdbd8
Avonts <vinialves022@gmail.com>
2020-04-13 22:38:36 -0300

    Adicionando a entidade User e o Resource

---
+++ src/main/java/com/course/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Maria Luisa", "maria@gmail.com", "999999999", "123456");
+		return ResponseEntity.ok().body(u);
+	}
+	
+}
