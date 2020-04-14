111ff0174a7360b019aefd9909f4184e3c631a98
Avonts <vinialves022@gmail.com>
2020-04-14 16:07:14 -0300

    Adicionando o endPoint Products

---
+++ src/main/java/com/course/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+	
+}
