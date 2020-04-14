7acfbcf37f9b8fb1b0e1bef2fcd751a6146753d7
Avonts <vinialves022@gmail.com>
2020-04-13 23:13:14 -0300

    Adicionando as dependencias e configurações do H2

--- src/main/java/com/course/entities/User.java
+++ src/main/java/com/course/entities/User.java
@@ -2,20 +2,28 @@ package com.course.entities;
 
 import java.io.Serializable;
 
-public class User implements Serializable{
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
 	private String phone;
 	private String password;
-	
+
 	public User() {
 
 	}
-	
+
 	public User(Long id, String name, String email, String phone, String password) {
 		this.id = id;
 		this.name = name;
@@ -88,5 +96,5 @@ public class User implements Serializable{
 			return false;
 		return true;
 	}
-	
+
 }
