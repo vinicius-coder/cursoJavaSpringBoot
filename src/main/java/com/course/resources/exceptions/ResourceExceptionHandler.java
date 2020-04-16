f436ef84f0ed5c6991fcda7e480e6d4d4442de76
Avonts <vinialves022@gmail.com>
2020-04-16 00:14:54 -0300

    Adicioanando exception 400 para o User DeleteById

--- src/main/java/com/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/course/resources/exceptions/ResourceExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.course.services.exceptions.DataBaseException;
 import com.course.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
@@ -21,5 +22,14 @@ public class ResourceExceptionHandler {
 		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
+	
+	@ExceptionHandler(DataBaseException.class)
+	public ResponseEntity<StandardError> DataBase(DataBaseException e, HttpServletRequest request) {
+		String error = "Database error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
 
 }
